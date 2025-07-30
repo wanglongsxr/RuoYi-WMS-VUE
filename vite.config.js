685c4c4371dfd59e2595de523dd1cadcadc5bb42
wanglong <long.wang@yingxiong.com>
2025-07-30 13:04:28 +0800

    本地开发配置

--- vite.config.js
+++ vite.config.js
@@ -30,7 +30,7 @@ export default defineConfig(({ mode, command }) => {
       proxy: {
         // https://cn.vitejs.dev/config/#server-proxy
         '/dev-api': {
-          target: 'http://localhost:8080',
+          target: 'http://47.83.155.93:8080',
           changeOrigin: true,
           rewrite: (p) => p.replace(/^\/dev-api/, '')
         }
